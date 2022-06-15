8622ae417f48a624ea86be2a205ca62a2ca9716d
KJ Johnson <kjohns95@emich.edu>
2022-06-15 18:55:24 -0400

    IN CLASS - added math.abs ti formatCurrency function

--- src/js/money-functions.js
+++ src/js/money-functions.js
@@ -2,7 +2,7 @@ function formatCurrency(amount) {
   if (amount >= 0) {
     return "$" + amount.toFixed(2);
   } else {
-    return "-$" + -amount.toFixed(2);
+    return "-$" + Math.abs(amount).toFixed(2);
   }
 }
 
